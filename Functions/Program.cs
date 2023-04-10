f7877787727a805a1f010a76f5d79fb3aac00f45
Igor Pr <igor.prokofjev@tktk.ee>
2023-04-10 10:26:14 +0300

    add week7functions code

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Are You coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else 
+{
+    PrintGoodbye();
+}
+
+
+static void PrintHello() 
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye() 
+{
+    Console.WriteLine("See You later, alligator!");
+}
---
+++ Test7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Choose: F/C");
+string userChoice = Console.ReadLine();
+Console.WriteLine("Enter the temperature:");
+int userTemp = Int32.Parse(Console.ReadLine());
+
+if (userChoice == F) 
+{
+
+}
